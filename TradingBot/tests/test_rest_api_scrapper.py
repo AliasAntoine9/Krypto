e741445a4f5b02d9b30e6d517d1317afd7eca4f7
ALIAS Antoine <alias.antoine@orange.fr>
2022-05-08 23:13:50 +0200

    [UnitTests] Beginning of their implementation

--- TradingBot/tests/test_rest_api_scrapper.py
+++ TradingBot/tests/test_rest_api_scrapper.py
@@ -1,6 +1,21 @@
 import unittest
+from src.scrapping.rest_api_scrapper import RestApiScrapper
 
-class TestScrapper:
+class TestScrapper(unittest.TestCase):
+	"""This class is a first try to implement unit tests"""
+
+	def setUp(self):
+		"""Arrange environment"""
+		print("Avant le test")
+		scrapper = RestApiScrapper("VET")
+	
 	def test_get_candles_return_json(self):
 		"""test_get_candles_return_json"""
-		
+		return
+
+	def tearDown(self):
+		"""Assert everything goes well"""
+		print("Après le test")
+
+if __name__ == "__main__":
+	unittest.main()
