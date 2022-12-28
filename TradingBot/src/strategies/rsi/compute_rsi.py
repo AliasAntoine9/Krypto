18eba410b1c7e7cd61a22228342f373097da67c1
ALIAS Antoine <alias.antoine@orange.fr>
2022-12-28 17:28:59 +0100

    [COMPUTE RSI] vectorise up and down columns computation

--- TradingBot/src/strategies/rsi/compute_rsi.py
+++ TradingBot/src/strategies/rsi/compute_rsi.py
@@ -2,55 +2,72 @@ import numpy as np
 
 
 class ComputeRsi:
-    def __init__(self, df_candles):
-        self.df_candles = df_candles
-        self.df_candles["close"] = self.df_candles["close"].astype("float", errors="raise")
+    def __init__(self, candles):
+        self.candles = candles
         self.multiplier = 2 / (1+14)
         self.inv_multiplier = 1 - self.multiplier
 
+    def compute_rsi(self):
+        """Main method of ComputerRsi object"""
+        self.create_close_columns()
+        self.create_up_and_down_columns()
+        self.create_exponential_average()
+        self.create_rs_and_rsi()
+
+    def create_close_columns(self):
+        """
+        This method is changing the type of 'close' column and adding 'close_lag' column
+        close_lag has same values than close column, but the values are lagged with 1 period
+        """
+        self.candles["close"] = self.candles["close"].astype("float", errors="raise")
+        self.candles["close_lag"] = self.candles["close"].shift(1)
+
     def create_up_and_down_columns(self):
-        list_up = []
-        list_down = []
+        """Create up and down columns which will be used to compute exponential average column"""
+        # Unload self
+        candles = self.candles.copy()
 
-        for i, elem in self.df_candles.iterrows():
-            if i != 0:
-                if self.df_candles["close"][i] > self.df_candles["close"][i-1]:
-                    list_up.append(
-                        self.df_candles["close"][i] - self.df_candles["close"][i-1]
-                    )
-                    list_down.append(0)
-                else:
-                    list_up.append(0)
-                    list_down.append(
-                        self.df_candles["close"][i-1] - self.df_candles["close"][i]
-                    )
-            else:
-                list_up.append(np.nan)
-                list_down.append(np.nan)
+        # Remove first line
+        candles = candles.loc[1:]
 
-        self.df_candles["up"] = list_up
-        self.df_candles["down"] = list_down
+        # Create a mask which keep only up moves
+        mask = candles["close"] > candles["close_lag"]
 
-    def compute_exponential_average(self):
+        # Add up column
+        candles.loc[mask, "up"] = candles["close"] - candles["close_lag"]
+        candles.loc[~mask, "up"] = 0.0
+
+        # Add down column
+        candles.loc[~mask, "down"] = candles["close_lag"] - candles["close"]
+        candles.loc[mask, "down"] = 0.0
+
+        # Load self
+        self.candles = candles
+
+    def create_exponential_average(self):
+        """
+        This method is adding 2 columns to candles df: exp_avg_up and exp_avg_down
+        These columns will be used to compute rs (Relative Strength) column
+        """
         i = 0
         list_exp_avg_up = []
         list_exp_avg_down = []
 
-        while i < self.df_candles.shape[0]:
+        while i < self.candles.shape[0]:
             if i > 15:
                 list_exp_avg_up.append(
-                    (self.df_candles["up"][i] * self.multiplier) + (list_exp_avg_up[i-1] * self.inv_multiplier)
+                    (self.candles["up"][i] * self.multiplier) + (list_exp_avg_up[i-1] * self.inv_multiplier)
                 )
                 list_exp_avg_down.append(
-                    (self.df_candles["down"][i] * self.multiplier) + (list_exp_avg_down[i-1] * self.inv_multiplier)
+                    (self.candles["down"][i] * self.multiplier) + (list_exp_avg_down[i-1] * self.inv_multiplier)
                 )
                 i += 1
             elif i == 15:
                 list_exp_avg_up.append(
-                    np.mean(self.df_candles["up"][1:15])
+                    np.mean(self.candles["up"][:15])
                 )
                 list_exp_avg_down.append(
-                    np.mean(self.df_candles["down"][1:15])
+                    np.mean(self.candles["down"][:15])
                 )
                 i += 1
             else:
@@ -58,14 +75,12 @@ class ComputeRsi:
                 list_exp_avg_down.append(np.nan)
                 i += 1
 
-        self.df_candles["exp_avg_up"] = list_exp_avg_up
-        self.df_candles["exp_avg_down"] = list_exp_avg_down
+        self.candles["exp_avg_up"] = list_exp_avg_up
+        self.candles["exp_avg_down"] = list_exp_avg_down
 
-    def compute_rsi_column(self):
-        self.df_candles["rs"] = self.df_candles["exp_avg_up"] / self.df_candles["exp_avg_down"]
-        self.df_candles["rsi"] = 100 - (100 / (1 + self.df_candles["rs"]))
-
-    def compute_rsi(self):
-        self.create_up_and_down_columns()
-        self.compute_exponential_average()
-        self.compute_rsi_column()
+    def create_rs_and_rsi(self):
+        """
+        This method is adding 2 columns to candles df: rs (Relative Strength) and rsi (Relative Strength Index)
+        """
+        self.candles["rs"] = self.candles["exp_avg_up"] / self.candles["exp_avg_down"]
+        self.candles["rsi"] = 100 - (100 / (1 + self.candles["rs"]))
